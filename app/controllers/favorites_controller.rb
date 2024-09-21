e2ff23ca12fb0b67ff2bd09d8df0c21691314545
nana <sss1230871997@gmail.com>
2024-09-21 21:23:51 +0900

    task

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,22 @@
 class FavoritesController < ApplicationController
 
-  def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    def create
+      @blog = Blog.find(params[:blog_id])
+      current_user.favorites.create(blog_id: @blog.id)
+      respond_to do |format|
+        format.html { redirect_to blogs_path}
+        format.js
+    end
   end
 
-  def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    def destroy
+      favorite = current_user.favorites.find_by(id: params[:id])
+      @blog_id = favorite.blog_id
+      favorite.destroy
+      
+      respond_to do |format|
+        format.html { redirect_to blogs_path}
+        format.js
+    end
   end
-
 end
